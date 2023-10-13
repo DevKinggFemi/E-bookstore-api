645831cf81c9e0a9a9848c2e928b378d2b899c0d
DevKinggFemi <mayowa.odunsi01@gmail.com>
2023-10-13 06:30:36 -0700

    new

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res,next)=> {
     next();
 })
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://e-bookstore-test.vercel.app'
 }));
 mongoose.connect(process.env.MONGO_URL
 ).then(()=>console.log("DBconnection Successful")).catch((err)=> console.log(err));
